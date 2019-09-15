16a5009f924e24c1ce85f175cb36f706855f087e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 16:25:18 -0400

    create and setup basic bot structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const clientbot = new Discord.Client();
+
+const token = 'insert token here';
+clientbot.login(token);
+
+clientbot.on('message', msg=>{
+    if (msg.content ===  "cmd"){
+        msg.reply('cmd test confirmed!');
+    }
+})
+
+
+clientbot.on('ready', ()=>{
+    console.log('Gwatch Bot is ready to go!')
+
+})
