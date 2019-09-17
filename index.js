98ba3c7d0b99905d165fae6eb6fa84fe2b85ee71
BuildTools <pruthvipalanpurwala@gmail.com>
2019-09-17 14:50:21 -0400

    add base kick command

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 
 const Discord = require('discord.js');
 
+
+
 const config = require('./config.json');
 const clientbot = new Discord.Client();
+
+const prefix = config.prefix;
 
 clientbot.login(config.token);
 
 clientbot.on('message', msg=>{
-    if (msg.content ===  "cmd"){
-        msg.reply('cmd test confirmed!');
+
+    let cmdArgs = msg.content.substring(prefix.length).split(" ");
+
+    switch(cmdArgs[0]) {
+        case 'cmd':
+            msg.reply('cmd test confirmed');
+        break;
+
+        case 'kick':
+            if(!cmdArgs[1]){
+                msg.channel.send("No user specified");
+            }
+        break;
+
     }
-})
+});
 
 
 clientbot.on('ready', ()=>{
