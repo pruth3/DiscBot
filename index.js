2b4b63eda1aa46a45202f23ce54602da4340691f
BuildTools <pruthvipalanpurwala@gmail.com>
2019-09-19 13:26:25 -0400

    add magic 8 ball funcitonality via m8

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ clientbot.login(config.token);
 
 clientbot.on('message', msg=>{
 
+    let magic8ans = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don\'t count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    ];
     let cmdArgs = msg.content.substring(prefix.length).split(" ");
 
     switch(cmdArgs[0]) {
@@ -36,6 +58,17 @@ clientbot.on('message', msg=>{
                 }
             }
         break;
+
+        case 'm8':
+            if (!cmdArgs[1]){
+                msg.channel.send("Please ask a question")
+                break;
+            }
+            else{
+                var replyNum = Math.floor(Math.random() * magic8ans.length);
+                msg.reply(magic8ans[replyNum]);
+
+            }
 
     }
 });
